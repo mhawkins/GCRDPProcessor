1aae7e77ab24684bd7f8977a4c2d73340e2189fb
Matt Hawkins <matthew.b.hawkins@gmail.com>
2013-11-21 14:40:14 -0500

    Initial Commit

---
+++ GCRDPProcessor/GCRDPProcessor.h
@@ -0,0 +1,13 @@
+//
+//  GCRDPProcessor.h
+//  GCRDPProcessor
+//
+//  Created by Matt Hawkins on 11/21/13.
+//  Copyright (c) 2013 Matt Hawkins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCRDPProcessor : NSObject
+
+@end
---
+++ GCRDPProcessor/GCRDPProcessor.m
@@ -0,0 +1,13 @@
+//
+//  GCRDPProcessor.m
+//  GCRDPProcessor
+//
+//  Created by Matt Hawkins on 11/21/13.
+//  Copyright (c) 2013 Matt Hawkins. All rights reserved.
+//
+
+#import "GCRDPProcessor.h"
+
+@implementation GCRDPProcessor
+
+@end
